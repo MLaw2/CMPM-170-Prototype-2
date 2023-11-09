594b924fb64fae55b594f59e2384a39e0ea68947
486ben <zhuan152@ucsc.edu>
2023-11-09 15:19:02 -0800

    update

--- docs/procrastinator/main.js
+++ docs/procrastinator/main.js
@@ -47,36 +47,52 @@ options = {
   viewSize: { x: 100, y: 100 },
 };
 
-let y;
+let y1;
+let y2;
 let vy;
 let isJumping;
 let spikes;
-let spikeAddDist;
+let spikeAddDist1;
+let spikeAddDist2;
 let scrolling;
+let jumptimes;
 
 function update() {
   if (!ticks) {
-    y = vy = 0;
+    y1 = y2 = vy = 0;
     isJumping = false;
     spikes = [];
-    spikeAddDist = 0;
+    spikeAddDist1 = 0;
+    spikeAddDist2 = 15;
     scrolling = 1;
+    jumptimes = 0;
   }
 
 
   scrolling = difficulty;
   score += scrolling / 10;
-  spikeAddDist -= scrolling;
+  spikeAddDist1 -= scrolling;
+  spikeAddDist2 -= scrolling;
 
   //enemy(for us is a book)
-  if (spikeAddDist < 0) {
+  if (spikeAddDist1 < 0) {
 
     //enemy y axis
-    const y = rnd() < 1 ? (rnd() < 0.8 ? 92 : 50) : rnd(92, 50);
+    const y1 = rnd() > 1 ? (rnd() < 0.8 ? 92 : 50) : rnd(92, 50);
     //? (rnd() < 0.5 ? 8 : 92) : rnd(8, 92)
-    spikes.push({ p: vec(103, y) });
+    spikes.push({ p: vec(103, y1) });
     //distance
-    spikeAddDist += rnd(10, 100);
+    spikeAddDist1 += rnd(10, 100);
+  }
+
+  if (spikeAddDist2 < 0) {
+
+    //enemy y axis
+    const y2 = rnd() < 1 ? (rnd() < 0.8 ? 59 : 0) : rnd(59, 0);
+    //? (rnd() < 0.5 ? 8 : 92) : rnd(8, 92)
+    spikes.push({ p: vec(103, y2) });
+    //distance
+    spikeAddDist2 += rnd(60, 100);
   }
   color("yellow");
 
@@ -89,14 +105,29 @@ function update() {
   if (!isJumping && input.isPressed) {
     play("powerUp");
     isJumping = true;
-    vy = 4;
+    vy = 2;
+    jumptimes = 1;
   }
 
+  //jump
   if (isJumping) {
     vy -= input.isPressed ? 0.1 : 0.3;
-    y += vy;
-    if (y < 0) {
-      y = 0;
+    y1 += vy;
+    y2 += vy;
+    //double jump
+    // if (input.isPressed || jumptimes < 2) {
+    //   play("powerUp");
+    //   isJumping = true;
+    //   vy = 2;
+    //   jumptimes = 2;
+    //   // if(jumptimes == 2)
+    //   // {
+    //   //   isJumping = false;
+    //   // }
+    // }
+    if (y1 < 0 || y2 < 0) {
+      y1 = 0;
+      y2 = 0;
       isJumping = false;
     }
   }
@@ -105,7 +136,7 @@ function update() {
   const c = isJumping ? "c" : addWithCharCode("a", floor(ticks / 15) % 2);
   if (
     // char(c, 50, 8 + y, { mirror: { y: -1 } }).isColliding.char.d ||
-    char(c, 50, 92 - y).isColliding.char.d
+    char(c, 50, 92 - y1).isColliding.char.d 
   ) {
     play("explosion");
     end();
@@ -115,6 +146,7 @@ function update() {
   rect(0, 0, 99, 5);
   rect(0, 95, 99, 5);
 }
+
 
 
 
